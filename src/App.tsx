eb3a1dce0f54707f925e287342c724b1a713f95a
Michael David Kuckuk <mdk@kuckuk.com>
2020-04-20 19:51:13 +0200

    Add persistence in browser (#14)
    * Add localForage persistence via redux-persist

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import React from 'react';
 import { Provider } from 'react-redux'
 import { store } from './store'
 import { CharacterSheet } from './components/CharacterSheet/CharacterSheet'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+const persistor = persistStore(store)
 
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <CharacterSheet/>
-    </Provider>
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <CharacterSheet/>
+      </Provider>
+    </PersistGate>
   );
 }
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,18 @@
 import { createStore } from 'redux'
 import { devToolsEnhancer } from 'redux-devtools-extension'
 import { RootReducer } from './root-reducer'
+import localForage from 'localforage'
+import { persistReducer } from 'redux-persist'
+
+
+const persistenceConfig = {
+  key: 'pathfinder-character-sheet',
+  storage: localForage,
+}
+
+const persistedReducer = persistReducer(persistenceConfig, RootReducer)
 
 export const store = createStore(
-  RootReducer,
+  persistedReducer,
   devToolsEnhancer({}),
 )
